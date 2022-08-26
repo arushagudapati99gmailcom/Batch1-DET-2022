15bd38706bb61d5fd6fceab8665f0141a4d03098
arushagudapati99gmailcom <105437036+arushagudapati99gmailcom@users.noreply.github.com>
2022-08-26 16:35:37 +0530

    Add files via upload

---
+++ Testemp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Testemp
+    {
+        public static void Main()
+        {
+            emp e = new emp(22795, "arusha", new DateOnly(2020, 8, 1));
+            Console.WriteLine($"years of experience{e.GetYearsofExp()}");
+        }
+    }
+}

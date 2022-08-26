406f3a5551ea14514eee3a97abc687586901eddc
arushagudapati99gmailcom <105437036+arushagudapati99gmailcom@users.noreply.github.com>
2022-08-26 16:24:44 +0530

    Add files via upload
    circle file is added

---
+++ circle/circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class circle
+    {
+        int radius;
+        public circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public double areaofcircle()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}

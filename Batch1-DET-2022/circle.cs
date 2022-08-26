afa3847474f44f4404f84033ffafe4de840db810
arusha <arusha.gudapati99@gmail.com>
2022-08-26 18:03:32 +0530

    area of circle

---
+++ Batch1-DET-2022/circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class circle
+    {
+
+        int radius;
+        public circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public double areaofcircle()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
+
---
+++ Batch1-DET-2022/testcircle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class testcircle
+    {
+        public static void Main()
+        {
+            circle c = new circle(50);
+            Console.WriteLine($"area of circle is {c.areaofcircle()}");
+        }
+    }
+}
+    
+
